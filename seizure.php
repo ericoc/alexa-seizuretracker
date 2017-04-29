85181bd80d03bf312bdd5a4570489200fd08b00e
Eric OC <eric@ericoc.com>
2017-04-29 17:22:11 -0400

    re-order everything in the main file to possible by more efficient...ish

--- seizure.php
+++ seizure.php
@@ -1,21 +1,23 @@
 <?php
 
-require_once('alexa.func.php');
-require_once('seizure.events.php');
-
 // Get the input from Alexa and JSON-decode it
 $input = json_decode(file_get_contents("php://input"));
 
-// Continue with finding the user and handling intent assuming we have valid input
+// Proceed if it is a somewhat valid request
 if ( (isset($input->session->user->userId)) && (!empty($input->session->user->userId)) && (isset($input->request->intent)) && (isset($input->request->intent->name)) ) {
 
-	error_log(print_r($input->session->user->accessToken, true));
-	error_log(print_r($input->request->intent, true));
+	// Tell the user to link their SeizureTracker account if no accessToken was found or their accessToken is not a string
+	if ( (!isset($input->session->user->accessToken)) || (empty($input->session->user->accessToken)) || (!is_string($input->session->user->accessToken)) ) {
+		linkMessage('Sorry, but please go to the Alexa website or app to link your SeizureTracker account.');
 
-	// Get user "accessToken" from Alexa
-	if ( (isset($input->session->user->accessToken)) && (is_string($input->session->user->accessToken)) ) {
+	} else {
 
-		// Handle the event based on the intent sent from Alexa
+		// Logging for debugging
+		error_log(print_r($input->session->user->accessToken, true));
+		error_log(print_r($input->request->intent, true));
+
+		// Include required functions and handle the event based on the intent sent from Alexa
+		require_once('seizure.events.php');
 		$handle_seizure = handle_seizure($input->session->user->accessToken, $input->request->intent);
 
 		// Set the message awkwardly
@@ -23,21 +25,19 @@ if ( (isset($input->session->user->userId)) && (!empty($input->session->user->us
 		if ( (isset($handle_seizure)) && (is_string($handle_seizure)) ) {
 			$message = $handle_seizure;
 
-		// Otherwise there was an error adding or finding/updating the seizure
+		// Otherwise there was some unknown error
 		} else {
 			$message = 'Sorry. There was an unknown error.';
 		}
-
-	// Otherwise, there was an error finding the users token and they probably need to link their account
-	} else {
-		linkMessage('Sorry, but please go to the Alexa website or app to link your SeizureTracker account.');
-		exit;
 	}
 
-// Otherwise, invalid input gets a default set of instructions for using this Alexa Skill
+// Otherwise, tell the user how to track a seizure upon invalid input
 } else {
 	$message = 'Please say, "Tell SeizureTracker to track a seizure", if you would like to track a seizure.';
 }
+
+// Include base Alexa response functions
+require_once('alexa.func.php');
 
 // The output is always JSON, return it!
 header('Content-Type: application/json;charset=UTF-8');

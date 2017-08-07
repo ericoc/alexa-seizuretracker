cfe9db522381b2c52f58d98dfa5c910b1f12f631
Eric OC <eric@ericoc.com>
2017-08-07 17:36:34 -0400

    move unused flash briefing function

--- alexa.func.php
+++ alexa.func.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+
+This is not currently used
+
+// Define a function to create JSON for Alexa Flash Briefing skills
+function briefing_out ($uid, $update_date, $title_text, $main_text, $redirect_url = null) {
+
+	// Create the short array to return for Flash Briefing skills, JSON encode it, and return it
+	$final = array('uid' => $uid, 'updateDate' => $update_date, 'titleText' => $title_text, 'mainText' => $main_text, 'redirectUrl' => $redirect_url);
+	$output = json_encode($final, JSON_PRETTY_PRINT);
+	return $output;
+}
+*/
+
 // Define a function to create JSON for Alexa to interpret
 function alexa_out ($speech, $card_title, $card_phrase, $reprompt_speech = null, $end_session = true, $redirect_url = null) {
 
@@ -21,20 +35,6 @@ function alexa_out ($speech, $card_title, $card_phrase, $reprompt_speech = null,
 	$output = json_encode($final, JSON_PRETTY_PRINT);
 	return $output;
 }
-
-/*
-
-This is not currently used
-
-// Define a function to create JSON for Alexa Flash Briefing skills
-function briefing_out ($uid, $update_date, $title_text, $main_text, $redirect_url = null) {
-
-	// Create the short array to return for Flash Briefing skills, JSON encode it, and return it
-	$final = array('uid' => $uid, 'updateDate' => $update_date, 'titleText' => $title_text, 'mainText' => $main_text, 'redirectUrl' => $redirect_url);
-	$output = json_encode($final, JSON_PRETTY_PRINT);
-	return $output;
-}
-*/
 
 // Define a function to create JSON for an Alexa card and instructions to users who have not linked their account
 function alexa_link_out ($speech) {
